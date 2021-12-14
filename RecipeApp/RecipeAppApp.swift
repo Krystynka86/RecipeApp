78c39e617d84805a28ff1bebbc500d5a4802ffbd
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2021-12-14 16:15:13 +0100

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Cristina on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
