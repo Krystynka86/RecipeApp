5a2276e888d021740869f847c71867ff26f466e8
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2021-12-17 17:30:47 +0100

    .make a portions .calculating ingrediens

--- RecipeApp/ViewModels/RecipeModel.swift
+++ RecipeApp/ViewModels/RecipeModel.swift
@@ -21,4 +21,82 @@ class RecipeModel: ObservableObject {
         
         //Set the recipes property
     }
-}
+    
+    static func getPortion(ingredient: Ingredient, recipeServings: Int, targetServings: Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        
+        if (ingredient.num != nil) {
+        
+        //Get a single serving size by multiplying denominator bythe recipe servings
+            // denominator = denominator * recipeServings - to samo co ponizej
+            denominator *= recipeServings
+        
+        //Get target portion by multiplying numerator by target servings
+            numerator *= targetServings
+        
+        //Reduce fraction by greatest common divisor
+            let divsior = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divsior
+            denominator /= divsior
+             
+        //Get the whole portion in numerator > denominator
+            if numerator >= denominator {
+                
+                //Calculated whole portions
+                wholePortions = numerator / denominator
+                
+                //Calculate the remainder/rest
+                numerator = numerator % denominator
+                
+                //Assign to portion string
+                portion += String(wholePortions)
+                 
+            }
+        
+        //Express the reminder as a fraction
+            if numerator > 0 {
+                
+                //Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+        
+        }
+        
+        if var unit = ingredient.unit {
+            
+            //If we need to pluralize
+            if wholePortions > 1 {
+                
+                //Calculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                    
+                   }
+                else {
+                    unit += "s"
+                }
+                    
+            }
+                
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? " " : ""
+            
+            return portion + unit 
+            
+            
+        }
+        
+        return portion
+    }
+ }
